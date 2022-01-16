78dbf03318895bc41f15b21ca35917644ec96884
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-01-16 09:10:00 +0900

    メモをDBへ保存

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // ログインしているユーザーの情報をビューへ渡す
+        $user = \Auth::user();
+        // メモ一覧を取得
+        // ASC=昇順,DESC=降順
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        dd($memos);
+        return view('home', compact('user', 'memos'));
     }
 
     public function create()
