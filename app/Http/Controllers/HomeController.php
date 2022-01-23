021fab93218fdeef254a0bd4c8d77d2b688ab2a1
jun-sugawara-2021 <j.sugawara@joa-holding.jp>
2022-01-23 11:01:50 +0900

    editを実装

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,4 +55,15 @@ class HomeController extends Controller
         // リダイレクト処理
         return redirect()->route('home');
     }
+
+    public function edit($id)
+    {
+        // 該当するIDのメモをデータベースから取得
+        $user = \Auth::user();
+        $memo = Memo::where('status', 1)->where('id', $id)->where('user_id', $user['id'])->first();
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        // dd($memo);
+        // 取得したメモをビューに渡す
+        return view('edit', compact('memo', 'user', 'memos'));
+    }
 }
