f85b609ac7b0200fc2c9a1a99bbe6d78b3b296d6
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-02-01 08:17:14 +0900

    route修正

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('root');
+Route::get('/', 'HomeController@index')->name('index');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/create', 'HomeController@create')->name('create');
 Route::post('store', 'HomeController@store')->name('store');
